1e0de1c9556e11dafeccf91ff547af351ca680ee
HEXXT <88392191+hexxt-git@users.noreply.github.com>
2024-10-28 22:56:00 +0100

    press anywhere

--- game_over.gd
+++ game_over.gd
@@ -4,7 +4,7 @@ func _ready() -> void:
 	set_process_input(true)
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventKey and event.pressed:
+	if event is InputEventMouseButton and event.pressed:
 		restart_game()
 
 func restart_game():
